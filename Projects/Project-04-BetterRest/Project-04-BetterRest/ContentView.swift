58e4f8eb24eff76963850c4ea64948ea069f3ff5
Laura Coronel <laura.ab.coronel@gmail.com>
2023-07-28 20:53:16 -0700

    Imported the ML model and got the app to work

--- Projects/Project-04-BetterRest/Project-04-BetterRest/ContentView.swift
+++ Projects/Project-04-BetterRest/Project-04-BetterRest/ContentView.swift
@@ -5,12 +5,17 @@
 //  Created by Laura Coronel on 7/28/23.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
 	@State private var wakeUP = Date.now
 	@State private var sleepAmount = 8.0
 	@State private var coffeeAmount = 1
+	
+	@State private var alertTitle = ""
+	@State private var alertMessage = ""
+	@State private var showingAlert = false
 	
     var body: some View {
 		NavigationStack {
@@ -35,11 +40,34 @@ struct ContentView: View {
 			.toolbar {
 				Button("Calculate", action: calculateBedtime)
 			}
+			.alert(alertTitle, isPresented: $showingAlert) {
+				Button("OK") {}
+			} message: {
+				Text(alertMessage)
+			}
 		}
     }
 	
 	func calculateBedtime() {
-		// hi
+		do {
+			let config = MLModelConfiguration() // Incase we need to enable a handful of options
+			let model = try SleepCalculator(configuration: config) // will read in all the data and output result
+			
+			let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUP)
+			let hour = (components.hour ?? 0) * 60 * 60 // in seconds
+			let minute = (components.minute ?? 0) * 60 // in seconds
+			
+			let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+			
+			let sleepTime = wakeUP - prediction.actualSleep
+			alertTitle = "Your ideal betime is..."
+			alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+			
+		} catch {
+			alertTitle = "Error"
+			alertMessage = "Sorry, there was a problem calculating your bedtime."
+		}
+		showingAlert = true
 	}
 }
 
