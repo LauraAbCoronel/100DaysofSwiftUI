dee6c19b5f597067472453ad14e80cd190a0ea43
Laura Coronel <laura.ab.coronel@gmail.com>
2023-08-06 10:22:33 -0700

    Add space theme UI

--- Challenges/MultiplicationTester/MultiplicationTester/SettingsView.swift
+++ Challenges/MultiplicationTester/MultiplicationTester/SettingsView.swift
@@ -1,13 +1,15 @@
 //
-//  QuestionGeneratorView.swift
+//  SettingsView.swift
 //  MultiplicationTester
 //
 //  Created by Laura Coronel on 8/5/23.
 //
 
+// <a href="https://www.flaticon.com/free-icons/launch" title="launch icons">Launch icons created by Iconic Panda - Flaticon</a>
+
 import SwiftUI
 
-struct QuestionGeneratorView: View {
+struct SettingsView: View {
 	@Binding var practiceNumber: Int
 	@Binding var numOfQuestions: Int // 0 is 5, 1 is 10 and 2 is 15
 	@Binding var isPlaying: Bool
@@ -35,8 +37,8 @@ struct QuestionGeneratorView: View {
 }
 
 #Preview {
-	QuestionGeneratorView(practiceNumber: .constant(5), numOfQuestions: .constant(0), isPlaying: .constant(false), generateQnA: {
+	SettingsView(practiceNumber: .constant(5), numOfQuestions: .constant(0), isPlaying: .constant(false), generateQnA: {
 		print("hi")
 	})
-//		QuestionGeneratorView(practiceNumber: .constant(5), numOfQuestions: .constant(0), isPlaying: .constant(false), questions: .constant(["2 X 4", "3 X 1", "4 X 1", "5 X 2", "4 X 5"]), answers: .constant([8, 3, 4, 10, 20]), choices: .constant([[8, 4, 12], [3, 0, 6], [4, 0, 8], [10, 5, 15], [20, 15, 25]]))
+//		SettingsView(practiceNumber: .constant(5), numOfQuestions: .constant(0), isPlaying: .constant(false), questions: .constant(["2 X 4", "3 X 1", "4 X 1", "5 X 2", "4 X 5"]), answers: .constant([8, 3, 4, 10, 20]), choices: .constant([[8, 4, 12], [3, 0, 6], [4, 0, 8], [10, 5, 15], [20, 15, 25]]))
 }
